be0eb24b48a3c37c4d1a4cb723365bb6de59282b
jvnguyen30 <40557863+jvnguyen30@users.noreply.github.com>
2018-06-27 15:49:55 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var num = 1 + 1;
 //document.getElementById("demo").innerHTML = num;
-window.alert("Harry Potter and the Goblet of Fire");
+//window.alert("Harry Potter and the Goblet of Fire");
 function myFunction()
 {
   var x = document.getElementById("myTextArea").value;
