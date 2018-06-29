ebf0c62e00ea0d9391501a301ac8b75c92849d99
jvnguyen30 <40557863+jvnguyen30@users.noreply.github.com>
2018-06-29 09:10:41 -0500

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ function myFunction()
   //var num = 2;
   //var numAsString = num.toString();
   //var str = "myTextArea" + numAsString;
-  var x = document.getElementById(str).value;
+  var x = document.getElementById("myTextArea2").value;
   var empty = " ";
   if(x === "")
   {
