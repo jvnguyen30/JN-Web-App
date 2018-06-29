3dff600fd4901fa05e1abb5f2c7c5fd8004259ff
jvnguyen30 <40557863+jvnguyen30@users.noreply.github.com>
2018-06-28 21:57:33 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function myFunction()
 {
-  var x = document.getElementById("myTextArea").value;
-  var y = parseInt(x) + 2;
-  document.getElementById("demo").innerHTML = y;
+  var x = document.getElementById("myTable").rows[0].cells[0].value;
+  x = parseInt(x) + 2;
+  document.getElementById("demo").innerHTML = x;
 }
