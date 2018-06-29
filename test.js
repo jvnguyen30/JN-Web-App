1292223b6aed4cb37639054dab16619a7ad1c367
jvnguyen30 <40557863+jvnguyen30@users.noreply.github.com>
2018-06-28 22:11:12 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function myFunction()
 {
-  var x = document.getElementById("myTable").rows[1].cells[1].innerHTML;
+  var x = document.getElementById("myTable").rows[1].cells[1].value;
   var y = parseInt(x);
   document.getElementById("demo").innerHTML = x;
 }
