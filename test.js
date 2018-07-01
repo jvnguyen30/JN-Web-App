080eb79f505b0adc46a2a9110e6a4bc0dc616648
jvnguyen30 <40557863+jvnguyen30@users.noreply.github.com>
2018-06-30 23:16:17 -0500

    Update test.js

--- test.js
+++ test.js
@@ -7,29 +7,74 @@ function myFunction()
   
   var printGrade = "";
   
-  //var numAsString = num.toString();
-  //var str = "myTextArea" + numAsString;
-  //var x = document.getElementById("myTextArea2").value;
   for(var x = 0; x < 7; x++)
   {
     let numAsString = num.toString();
     let str = "myTextArea" + numAsString;
     let credits = document.getElementById(str).value;
+    let points = 0;
+    
     num++; //iterate num to get grade
     numAsString = num.toString();
     str = "myTextArea" + numAsString;
     let grade = document.getElementById(str).value;
     num++; //iterate num for next iteration of loop
+    
     if(credits != "")
     {
       totalCredits += parseInt(credits);
     }
+    credits = parseInt(credits);
     if(grade != "")
     {
       printGrade += grade + " ";
+      switch(grade)
+      {
+        case "A":
+          points = credits * 4.0;
+          break;
+        case "A-":
+          points = credits * 3.667;
+          break;
+        case "B+":
+          points = credits * 3.333;
+          break;
+        case "B":
+          points = credits * 3.0;
+          break;
+        case "B-":
+          points = credits * 2.667;
+          break;
+        case "C+":
+          points = credits * 2.333;
+          break;
+        case "C":
+          points = credits * 2.0;
+          break;
+        case "C-":
+          points = credits * 1.667;
+          break;
+        case "D+":
+          points = credits * 1.333;
+          break;
+        case "D":
+          points = credits * 1.0;
+          break;
+        case "D-":
+          points = credits * 0.667;
+          break;
+        case "F":
+          points = 0;
+          break;
+        default:
+          points = 0;
+          break;
+      }
+      totalPoints += points;
     }
   }
   //var test = "LeBron";
+  gpa = totalPoints/totalCredits;
   document.getElementById("demo").innerHTML = totalCredits;
-  document.getElementById("demo2").innerHTML = printGrade;
+  document.getElementById("demo2").innerHTML = gpa;
 }
